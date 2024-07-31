453ab389809d291fbdf0aa89e79c35133cfb74bc
Anita Gandhi <anitagandhi1@gmail.com>
2024-07-31 12:48:50 -0500

    Add css, header, footer

--- portfolio_website/main.py
+++ portfolio_website/main.py
@@ -2,14 +2,19 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 
-
-
 app = Flask(__name__)
+
+this_year = date.today().year
 
 
 @app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', this_year=this_year)
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html', this_year=this_year)
 
 
 if __name__ == "__main__":
